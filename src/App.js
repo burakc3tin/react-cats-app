ba832f6e0a9cfca25f69a1446c21866f8ac732d7
burakc3tin <burakcetin9724@gmail.com>
2022-03-24 14:10:58 +0300

    cats api'den resimler çekiliyor ve her refresh'de yeni bir kedi resmi geliyor

--- src/App.js
+++ src/App.js
@@ -1,33 +1,41 @@
- 
-import {React,useEffect,useState} from "react";
- 
+import axios from "axios";
+import { React, useEffect, useState } from "react";
 
 function App() {
 
-  const [image,setImage] = useState([]);
-  
-  useEffect(()=>{
+  const [isLoading, setLoading] = useState(true);
+  const [image, setImage] = useState([]);
 
-    
-  },[])
+  useEffect(() => {
+    axios.get("https://api.thecatapi.com/v1/images/search").then(response => {
+      setImage(response);
+      setLoading(false);
+      console.log(image.data[0])
+    });
+  }, []);
+
+
+  if (isLoading) {
+    return <div className="App">Loading...</div>;
+  } //Burası loader ekranı
+
 
   return (
     <div style={styles.mainFrame} >
-     
-
-    <img  src="https://via.placeholder.com/350x150"/>
+      Test
+      <img src={image.data[0].url} />
 
     </div>
   );
 }
 
 const styles = {
- mainFrame : {
-  display:'flex',
-  alignItems:'center',
-  justifyContent:'center',
-  height: "100vh"
- }
+  mainFrame: {
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: "100vh"
+  }
 };
 
 export default App;
